6b8263060ce0cfa0aeeab1ec7379cd2ac9a0f870
Jacob1010-h <lordgaming1010@gmail.com>
2023-02-23 12:38:13 -0800

    Added in isFull(), closeIfWinner(), and isWinner() methods.
    Co-authored-by: @GalexY727

--- src/tictactoe/View.java
+++ src/tictactoe/View.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame{
+    private String turn = "X";
     private JPanel tictactoePanel;
     private JButton topMid;
     private JButton midMid;
@@ -24,39 +25,68 @@ public class View extends JFrame{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
-        String turn = "X";
-
         topMid.addActionListener(e -> {
             XorOCheck(topMid);
+            closeIfWinner();
         });
         midMid.addActionListener(e -> {
             XorOCheck(midMid);
+            closeIfWinner();
         });
         bottomMid.addActionListener(e -> {
             XorOCheck(bottomMid);
+            closeIfWinner();
         });
         rightMid.addActionListener(e -> {
             XorOCheck(rightMid);
+            closeIfWinner();
         });
         leftMid.addActionListener(e -> {
             XorOCheck(leftMid);
+            closeIfWinner();
         });
         rightTop.addActionListener(e -> {
             XorOCheck(rightTop);
+            closeIfWinner();
         });
         rightBottom.addActionListener(e -> {
             XorOCheck(rightBottom);
+            closeIfWinner();
         });
         leftTop.addActionListener(e -> {
             XorOCheck(leftTop);
+            closeIfWinner();
         });
         leftBottom.addActionListener(e -> {
             XorOCheck(leftBottom);
+            closeIfWinner();
         });
 
     }
 
+    public void closeIfWinner() {
+        if (!isWinner() && isFull()) {
+            JOptionPane.showMessageDialog(null, "Draw");
+            dispose();
+        }else if (isWinner()) {
+            JOptionPane.showMessageDialog(null, "Winner is " + (turn.equals("X") ? "O" : "X"));
+            dispose();
+        }
+    }
+
+    public boolean isFull() {
+        for (JButton button : new JButton[]{topMid, midMid, bottomMid, rightMid, leftMid, rightTop, rightBottom, leftTop, leftBottom}) {
+            if (button.getText().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public void XorOCheck(JButton button) {
+        if (!button.getText().isEmpty()) {
+            return;
+        }
         switch (turn) {
             case "X" -> {
                 button.setText("X");
@@ -67,5 +97,49 @@ public class View extends JFrame{
                 turn = "X";
             }
         }
+    }
+
+    public boolean isWinner(){
+        if (    topMid.getText().equals(midMid.getText()) &&
+                midMid.getText().equals(bottomMid.getText()) &&
+                !topMid.getText().isEmpty())
+            return true;
+
+        if (    rightTop.getText().equals(rightMid.getText()) &&
+                rightMid.getText().equals(rightBottom.getText()) &&
+                !rightTop.getText().isEmpty())
+            return true;
+
+        if (    leftTop.getText().equals(leftMid.getText()) &&
+                leftMid.getText().equals(leftBottom.getText()) &&
+                !leftTop.getText().isEmpty())
+            return true;
+
+        if (    leftTop.getText().equals(midMid.getText()) &&
+                midMid.getText().equals(rightBottom.getText()) &&
+                !leftTop.getText().isEmpty())
+            return true;
+
+        if (    rightTop.getText().equals(midMid.getText()) &&
+                midMid.getText().equals(leftBottom.getText()) &&
+                !rightTop.getText().isEmpty())
+            return true;
+
+        if (    leftTop.getText().equals(topMid.getText()) &&
+                topMid.getText().equals(rightTop.getText()) &&
+                !leftTop.getText().isEmpty())
+            return true;
+
+        if (    leftMid.getText().equals(midMid.getText()) &&
+                midMid.getText().equals(rightMid.getText()) &&
+                !leftMid.getText().isEmpty())
+            return true;
+
+        if (    leftBottom.getText().equals(bottomMid.getText()) &&
+                bottomMid.getText().equals(rightBottom.getText()) &&
+                !leftBottom.getText().isEmpty())
+            return true;
+
+        return false;
     }
 }
